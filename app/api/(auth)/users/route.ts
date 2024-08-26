9182beb3d6a3b4497e9c25f922eecc6b365a8a3b
mikenegh <mikenegh@gmail.com>
2024-08-26 18:28:24 +0000

    PATCH

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -1,6 +1,11 @@
 import { NextResponse } from "next/server";
 import User from "@/lib/models/users";
 import connect from "@/lib/db";
+import {Types} from "mongoose"
+
+
+
+const ObjectId = require("mongoose").Types.ObjectId;
 
 export const GET = async () => {
   try {
@@ -23,3 +28,45 @@ export const POST = async (request: Request) => {
     return new NextResponse("Error in creating new user: " + error.message, { status: 500 });
   }
 };
+
+
+
+export const PATCH = async (request : Request) => {
+  try{
+    const body = await request.json();
+    
+    const {userId,newUserName} = body;
+    await connect();
+
+    if(!userId || !newUserName){
+      return new NextResponse(JSON.stringify({message: "Invalid Request"}), {status: 400})
+    }
+
+    if(!Types.ObjectId.isValid(userId)){
+      return new NextResponse(JSON.stringify({message: "Invalid User Id"}),{status:400})
+
+    }
+    const updateUser = await User.findOneAndUpdate({
+      _id: new ObjectId(userId)
+    },
+    {
+      $set: {
+        Username: newUserName
+      }
+    },
+    {
+      new: true
+    
+    })
+
+
+    if(!updateUser){
+      return new NextResponse(JSON.stringify({message: "User not updated"}),{status: 404})
+
+    }
+    return new NextResponse(JSON.stringify({message:"User Updated"}),{status:200})
+  }
+  catch(error: any){
+    return new NextResponse("Error: " + error.message, { status: 500 });
+  }
+}
